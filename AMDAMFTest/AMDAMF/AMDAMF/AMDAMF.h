84ee994607f0c2ea45e43ba53d4b922604481509
barry <barry.hung@centron.tw>
2017-07-20 16:59:51 +0800

    first commit

---
+++ AMDAMFTest/AMDAMF/AMDAMF/AMDAMF.h
@@ -0,0 +1,27 @@
+// AMDAMF.h
+
+#pragma once
+
+using namespace System;
+
+namespace AMDAMF {
+	////For C++ dll to C# testing
+	//public ref class Arithmetics
+	//{
+	//public:
+	//	// TODO: 在此加入這個類別的方法。
+	//	Arithmetics(){};
+	//	double Add(double x, double y);
+	//	double Subtract(double a, double b);
+	//	double Multiply(double x, double y);
+	//	double Divide(double a, double b);
+	//};
+
+
+	public ref class AmdEncoder
+	{
+	public:
+		void TestEncode();
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ AMDAMFTest/AMDAMFTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AMDAMF;
+
+namespace AMDAMFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetics test = new Arithmetics();
+            
+            //System.Diagnostics.Debug.WriteLine(test.Add(3, 2));
+
+            AmdEncoder amdEncoder = new AmdEncoder();
+            amdEncoder.TestEncode();
+        }
+    }
+}
